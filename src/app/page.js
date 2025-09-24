f2e888cdf1bb98077ca573927847c0eff58928f2
rolandaayo <ibiwoyeroland@gmail.com>
2025-09-24 10:16:04 +0100

    font fixed

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,17 @@
 import Navbar from "./components/Navbar";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
+import { Goldman } from "next/font/google";
+
+const goldman = Goldman({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-white flex flex-col">
+    <div className={`${goldman.className} min-h-screen bg-white flex flex-col`}>
       <Navbar />
       <main className="flex-grow">
         <Body />
