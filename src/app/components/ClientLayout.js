a2a68ea4ecc3df5f7c13204a229b672afcca4bf9
rolandaayo <ibiwoyeroland@gmail.com>
2025-11-12 16:31:23 +0100

    server

--- src/app/components/ClientLayout.js
+++ src/app/components/ClientLayout.js
@@ -2,38 +2,35 @@
 
 import { Toaster } from "react-hot-toast";
 import { CartProvider } from "../context/CartContext";
-import { AnimatePresence } from "framer-motion";
 import ActivityTracker from "./ActivityTracker";
 
 export default function ClientLayout({ children }) {
   return (
     <CartProvider>
       <ActivityTracker>
-        <AnimatePresence mode="wait">
-          <Toaster
-            position="top-right"
-            toastOptions={{
+        <Toaster
+          position="top-right"
+          toastOptions={{
+            duration: 3000,
+            style: {
+              background: "#363636",
+              color: "#fff",
+            },
+            success: {
               duration: 3000,
-              style: {
-                background: "#363636",
-                color: "#fff",
+              theme: {
+                primary: "#4aed88",
               },
-              success: {
-                duration: 3000,
-                theme: {
-                  primary: "#4aed88",
-                },
+            },
+            error: {
+              duration: 4000,
+              theme: {
+                primary: "#ff4b4b",
               },
-              error: {
-                duration: 4000,
-                theme: {
-                  primary: "#ff4b4b",
-                },
-              },
-            }}
-          />
-          {children}
-        </AnimatePresence>
+            },
+          }}
+        />
+        {children}
       </ActivityTracker>
     </CartProvider>
   );
